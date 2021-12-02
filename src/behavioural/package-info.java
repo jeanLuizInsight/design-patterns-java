c7daa0d3c9d9a7bd7896f1746c2226c59f24ffe4
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2021-12-02 09:40:14 -0300

    Atualizado leia-me dos pacotes das categorias das padrões.

--- src/behavioural/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Pacote que contém as implementações dos padrões comportamentais do catálogo de padrões GoF
- *
- *  Estes padrões são voltados aos algoritmos e a designação de responsabilidades entre objetos.
- *
- *  @author jean
- */
-package behavioural;
--- src/structural/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Pacote que contém as implementações dos padrões estruturais do catálogo de padrões GoF
- *
- *  Estes padrões explicam como montar objetos e classes em estruturas maiores mas
- *  ainda mantendo essas estruturas flexíveis e eficientes.
- *
- *  @author jean
- */
-package structural;
