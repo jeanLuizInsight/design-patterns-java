aae4309c248225de117fc0a317a50a7b8d05be59
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2021-12-08 11:00:54 -0300

    Criação do ponto de execução do programa para demonstração padrão Builder.

--- src/creational/builder/App.java
+++ src/creational/builder/App.java
@@ -1,10 +1,34 @@
 package creational.builder;
 
 /**
- * Classe de execuÃ§Ã£o das implementaÃ§Ãµes do padrÃ£o Builder.
+ * Classe de execuçãoo das implementações do padrão Builder.
  * @author <a href="mailto:jean.zanatta@unoesc.edu.br">Jean Luiz Zanatta</a>
  * @since 07/12/2021
  */
 public class App {
 
+	public static void main(final String[] args) {
+
+		/**
+		 * Instanciando três tipos de heróis com suas características
+		 */
+
+		final Heroi mago = new Heroi.Builder("Nicholas Flamel", Profissao.MAGO)
+				.comCorDoCabelo(CorDoCabelo.BRANCO)
+				.comArma(Arma.ARCO)
+				.build();
+		System.out.println(mago);
+
+		final Heroi guerreiro = new Heroi.Builder("Leonidas", Profissao.GUERREIRO)
+				.comCorDoCabelo(CorDoCabelo.MARROM)
+				.comArmadura(Armadura.METAL)
+				.comArma(Arma.ESPADA)
+				.build();
+		System.out.println(guerreiro);
+
+		final Heroi ladrao = new Heroi.Builder("Billy the Kid", Profissao.LADRAO)
+				.comArma(Arma.PUNHAL)
+				.build();
+		System.out.println(ladrao);
+	}
 }
