6aa9ad5e9e19e89ab6ae1f7d0a21119563e4b6c2
jl.zanatta92@yahoo.com.br <jl.zanatta92@yahoo.com.br>
2021-12-07 22:29:14 -0300

    Criação diagrama e leia-me para padrão criacional Builder.

---
+++ src/creational/builder/App.java
@@ -0,0 +1,10 @@
+package creational.builder;
+
+/**
+ * Classe de execução das implementações do padrão Builder.
+ * @author <a href="mailto:jean.zanatta@unoesc.edu.br">Jean Luiz Zanatta</a>
+ * @since 07/12/2021
+ */
+public class App {
+
+}
