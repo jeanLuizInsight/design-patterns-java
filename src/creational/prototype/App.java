cc2f1dd5b1588aa34269549b0828897168f1b35e
jl.zanatta92@yahoo.com.br <jl.zanatta92@yahoo.com.br>
2021-12-14 21:48:11 -0300

    Criação do pacote e modelagem de representação do padrão Prototype.

---
+++ src/creational/prototype/App.java
@@ -0,0 +1,13 @@
+package creational.prototype;
+
+/**
+ * Classe de execuçãoo das implementações do padrão Prototype.
+ * @author <a href="mailto:jean.zanatta@unoesc.edu.br">Jean Luiz Zanatta</a>
+ * @since 14/12/2021
+ */
+public class App {
+
+	public static void main(final String[] args) {
+
+	}
+}
