7b8637bc4f67bce2d90118780e827cae78faa0ce
jl.zanatta92@yahoo.com.br <jl.zanatta92@yahoo.com.br>
2021-12-16 20:29:53 -0300

    Criação do diagrama de classes para implementação do padrão method factory.

---
+++ src/creational/factory_method/App.java
@@ -0,0 +1,13 @@
+package creational.factory_method;
+
+/**
+ * Classe de execução das implementações do padrão Factory Method.
+ * @author <a href="mailto:jean.zanatta@unoesc.edu.br">Jean Luiz Zanatta</a>
+ * @since 16/12/2021
+ */
+public class App {
+
+	public static void main(final String[] args) {
+
+	}
+}
