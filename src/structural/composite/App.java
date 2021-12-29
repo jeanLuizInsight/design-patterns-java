a8c2218d6f5117a6d75f5b874d297aaf6249d757
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2021-12-29 16:32:00 -0300

    Adicionado descrição da implementação do padrão na classe que contém a entrada do programa.

--- src/structural/composite/App.java
+++ src/structural/composite/App.java
@@ -3,6 +3,13 @@ package structural.composite;
 import java.util.logging.Logger;
 
 /**
+ * O padrão Composite é um padrão de design de particionamento. O padrão Composite descreve que um
+ * grupo de objetos deve ser tratado da mesma maneira que uma única instância de um objeto. A intenção
+ * de um composite é "compor" objetos em estruturas de árvore para representar hierarquias parte-todo.
+ * Implementar o padrão Composite permite que os clientes tratem objetos e composições individuais
+ * uniformemente.
+ * Neste exemplo, temos frases compostas por palavras compostas por letras. Todos os objetos
+ * podem ser tratados pela mesma interface ({ @link LetraComposite}).
  * @author <a href="mailto:jean.zanatta@unoesc.edu.br">Jean Luiz Zanatta</a>
  * @since 29/12/2021
  */
